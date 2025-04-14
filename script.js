3093a7126ed0c403df493655ed3009684880808c
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-14 20:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.getElementById("counter").innerText = "0";
+
+document.getElementById("incrementBtn").addEventListener("click", () => {
+    let counterElement = document.getElementById("counter");
+    let currentValue = parseInt(counterElement.innerText);
+    alert(counterElement.innerText = currentValue + 1);
+	
+});
